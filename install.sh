21b97e73fbdfc7ce7b9f50e6177e8c9d578f1106
Jackson Cooper <jackson@jacksonc.com>
2022-03-15 14:20:40 +1100

    Finalise the install script and splitout base vs personal git configs

--- install.sh
+++ install.sh
@@ -2,41 +2,42 @@
 
 export DOTFILES_PATH=~/.dotfiles
 
-echo "=> Fetching latest dotfiles"
-git_status="$(git status -s 2>&1)"
-if [[ -n "$git_status" && "$1" != "--local" ]] ; then
-    echo "ERR: Unsaved changes"
-    echo "$git_status"
+home_dotfiles=(
+  ~/.aliases  
+  ~/.curlrc   
+  ~/.functions   
+  ~/.gitconfig   
+  ~/.osx   
+  ~/.zshrc   
+  ~/.iterm2_shell_integration.zsh   
+)
+
+# Delete existing dotfiles.
+for home_dotfile in ${home_dotfiles[@]} ; do
+  basename="$(basename "$home_dotfile")"
+  home_dotfile_path="$HOME/$basename"
+  if [[ -f "$home_dotfile_path" || -L "$home_dotfile_path" ]] ; then
+    echo "=> Deleting '$home_dotfile_path"
+    rm -f "$home_dotfile_path"
+  fi
+done
+
+# Check if any dotfile already exists.
+for dotfile_path in ${dotfile_paths[@]} ; do
+  basename="$(basename "$dotfile_path")"
+  dotfile_home_path="$HOME/$basename"
+  if [[ -f "$dotfile_home_path" || -L "$dotfile_home_path" ]] ; then
+    echo >&2 "ERROR: '$dotfile_home_path' already exists"
     exit 1
-fi
+  fi
+done
 
-if [[ "$1" != "--local" ]] ; then
-	git fetch
-	git pull
-fi
+echo '=> Linking dotfiles'
+for dotfile_path in ${home_dotfiles[@]} ; do
+  basename="$(basename "$dotfile_path")"
+  echo "Linking '$DOTFILES_PATH/$basename' -> '~/$basename'"
+  ln -s "$DOTFILES_PATH/$basename" ~/$basename
+done
 
-echo "=> Deleting existing dotfiles"
-rm ~/.aliases 2>/dev/null
-rm ~/.curlrc 2>/dev/null
-rm ~/.functions 2>/dev/null
-rm ~/.gitconfig 2>/dev/null
-rm ~/.gitconfig_system 2>/dev/null
-rm ~/.gitconfig_user 2>/dev/null
-rm ~/.osx 2>/dev/null
-rm ~/.zshrc 2>/dev/null
-rm ~/.zshrc 2>/dev/null
-rm ~/.iterm2_shell_integration.zsh 2>/dev/null
-
-echo "=> Linking dotfiles"
-ln -s --force "$DOTFILES_PATH/.aliases" ~/.aliases
-ln -s --force "$DOTFILES_PATH/.curlrc" ~/.curlrc
-ln -s --force "$DOTFILES_PATH/.functions" ~/.functions
-#ln -s --force "$DOTFILES_PATH/.gitconfig" ~/.gitconfig
-#ln -s --force "$DOTFILES_PATH/.gitconfig_system" ~/.gitconfig_system
-#ln -s --force "$DOTFILES_PATH/.gitconfig_user" ~/.gitconfig_user
-ln -s --force "$DOTFILES_PATH/.osx" ~/.osx
-ln -s --force "$DOTFILES_PATH/.zshrc" ~/.zshrc
-ln -s --force "$DOTFILES_PATH/vendor/.iterm2_shell_integration.zsh" ~/.iterm2_shell_integration.zsh
-
-echo "=> Re-loading ~/.zshrc"
+echo '=> Re-loading ~/.zshrc'
 source ~/.zshrc
