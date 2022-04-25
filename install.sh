405487ace97fdb2b76bcc0f826312660a67773c5
Jackson Cooper <jackson@jacksonc.com>
2022-04-25 21:03:30 +1000

    Vimrc cleanup

--- install.sh
+++ install.sh
@@ -29,34 +29,15 @@ brew install gnu-tar
 brew install go # Required for SQL language server.
 brew install grep
 brew install hashicorp/tap/terraform-ls
-brew install homebrew/cask-fonts/font-anonymice-powerline
-brew install homebrew/cask-fonts/font-dejavu-sans-mono-for-powerline
-brew install homebrew/cask-fonts/font-droid-sans-mono-for-powerline
-brew install homebrew/cask-fonts/font-fantasque-sans-mono
-brew install homebrew/cask-fonts/font-fira-code
-brew install homebrew/cask-fonts/font-fira-mono-for-powerline
-brew install homebrew/cask-fonts/font-fira-sans
-brew install homebrew/cask-fonts/font-hack
-brew install homebrew/cask-fonts/font-inconsolata-dz-for-powerline
-brew install homebrew/cask-fonts/font-inconsolata-for-powerline
-brew install homebrew/cask-fonts/font-inconsolata-g-for-powerline
-brew install homebrew/cask-fonts/font-input
+brew install homebrew/cask-fonts/font-droid-sans-mono-nerd-font
+brew install homebrew/cask-fonts/font-fantasque-sans-mono-nerd-font
+brew install homebrew/cask-fonts/font-fira-mono-nerd-font
+brew install homebrew/cask-fonts/font-hack-nerd-font
+brew install homebrew/cask-fonts/font-inconsolata-nerd-font
+brew install homebrew/cask-fonts/font-jetbrains-mono-nerd-font
 brew install homebrew/cask-fonts/font-liberation-mono-for-powerline
-brew install homebrew/cask-fonts/font-menlo-for-powerline
-brew install homebrew/cask-fonts/font-merienda
-brew install homebrew/cask-fonts/font-merienda-one
-brew install homebrew/cask-fonts/font-merriweather
-brew install homebrew/cask-fonts/font-meslo-for-powerline
-brew install homebrew/cask-fonts/font-meslo-lg
-brew install homebrew/cask-fonts/font-monofur-for-powerline
-brew install homebrew/cask-fonts/font-monoid
-brew install homebrew/cask-fonts/font-office-code-pro
-brew install homebrew/cask-fonts/font-oxygen
-brew install homebrew/cask-fonts/font-oxygen-mono
-brew install homebrew/cask-fonts/font-pt-mono
-brew install homebrew/cask-fonts/font-source-sans-pro
-brew install homebrew/cask-fonts/font-ubuntu
-brew install homebrew/cask-fonts/font-ubuntu-mono-derivative-powerline
+brew install homebrew/cask-fonts/font-mononoki-nerd-font
+brew install homebrew/cask-fonts/font-ubuntu-mono-nerd-font
 brew install htop
 brew install jesseduffield/lazygit/lazygit
 brew install jq
@@ -89,6 +70,7 @@ brew install wget
 brew install whois
 brew install yarn
 brew install z
+brew install zsh
 brew install zsh-autosuggestions
 
 # Install Brew cask packages.
@@ -97,7 +79,6 @@ brew install --cask dash
 brew install --cask dropbox
 brew install --cask evernote
 brew install --cask firefox
-brew install --cask font-hack-nerd-font
 brew install --cask homebrew/cask/docker
 brew install --cask insomnia
 brew install --cask iterm2
@@ -151,7 +132,7 @@ npm install -g neovim
 npm -g install lehre
 
 # Install coc.nvim plugins.
-nvim +'CocInstall -sync coc-css' +qall
+nvim -c +'CocInstall -sync coc-css' +qall
 nvim +'CocInstall -sync coc-dictionary' +qall
 nvim +'CocInstall -sync coc-docker' +qall
 nvim +'CocInstall -sync coc-eslint' +qall
@@ -176,6 +157,7 @@ nvim +CocUpdateSync +qall
 # Install Treesitter plugins.
 nvim +'TSInstall bash' +qall
 nvim +'TSInstall comment' +qall
+nvim +'TSInstall css' +qall
 nvim +'TSInstall graphql' +qall
 nvim +'TSInstall help' +qall
 nvim +'TSInstall html' +qall
@@ -186,7 +168,10 @@ nvim +'TSInstall json' +qall
 nvim +'TSInstall json5' +qall
 nvim +'TSInstall jsonc' +qall
 nvim +'TSInstall php' +qall
-nvim +'TSInstall phpdoc' # Mac ARM not supported yet. https://github.com/claytonrcarter/tree-sitter-phpdoc/issues/15
+# Mac ARM not yet supported yet. https://github.com/claytonrcarter/tree-sitter-phpdoc/issues/15
+if [[ $(uname -m) !== 'arm' ]] ; then
+  nvim +'TSInstall phpdoc'
+fi
 nvim +'TSInstall scss' +qall
 nvim +'TSInstall typescript' +qall
 nvim +'TSInstall vim' +qall
