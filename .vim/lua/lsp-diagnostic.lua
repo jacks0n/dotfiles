b1c8d4d8ecfd6fb758bdfd95bd759743eca52a5b
Jackson Cooper <jackson@jacksonc.com>
2022-04-25 18:55:11 +1000

    Add the Lua plugin configs, make the LSP config dynamic

---
+++ .vim/lua/lsp-diagnostic.lua
@@ -0,0 +1,31 @@
+local lsp_diagnostic = function()
+  local sign = function(opts)
+    vim.fn.sign_define(opts.name, {
+      texthl = opts.name,
+      text = opts.text,
+      numhl = ''
+    })
+  end
+
+  sign({name = 'DiagnosticSignError', text = '✘'})
+  sign({name = 'DiagnosticSignWarn', text = '▲'})
+  sign({name = 'DiagnosticSignHint', text = '⚑'})
+  sign({name = 'DiagnosticSignInfo', text = ''})
+
+  vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = false,
+      style = 'minimal',
+      border = 'rounded',
+      source = 'always',
+      header = '',
+      prefix = '',
+    },
+  })
+end
+lsp_diagnostic()
---
+++ .vim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,91 @@
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  snippet = {
+    expand = function(args)
+      vim.fn['UltiSnips#Anon'](args.body)
+    end,
+  },
+  completeopt = 'menu,menuone,noinsert',
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'copilot', group_index = 2 },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'cmp_tabnine' },
+    { name = 'fuzzy_path'},
+    { name = 'npm', keyword_length = 4 },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'buffer' },
+    { name = 'fuzzy_path'},
+  })
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'buffer' },
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lsp_install = require('nvim-lsp-installer')
+local nvim_lsp = require('lspconfig')
+for index, lspServer in pairs(lsp_install.get_installed_servers()) do
+  local lspServerName = lspServer['name']
+  nvim_lsp[lspServerName].setup(capabilities)
+end
