17ca75a5be5de85195390259737d671424b9d4f5
Jackson Cooper <jackson@jacksonc.com>
2023-02-26 13:39:34 +1100

    Remove some unused vim plugins.

--- .vim/lua/plugins/lspsaga.lua
+++ .vim/lua/plugins/lspsaga.lua
@@ -1,1 +1,12 @@
-require('lspsaga').setup({})
+require('lspsaga').setup({
+  symbol_in_winbar = {
+    enable = true,
+    separator = '  ',
+    ignore_patterns={},
+    hide_keyword = true,
+    show_file = true,
+    folder_level = 2,
+    respect_root = false,
+    color_mode = true,
+  }
+})
