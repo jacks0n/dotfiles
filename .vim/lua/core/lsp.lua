40f681734e86ee277247e6c05f308095331dd1f0
Jackson Cooper <jackson@jacksonc.com>
2025-10-30 21:06:19 +1100

    Vim updates

--- .vim/lua/core/lsp.lua
+++ .vim/lua/core/lsp.lua
@@ -43,7 +43,7 @@ require('mason-lspconfig').setup({
     'sqlls',
     'intelephense',
     'omnisharp',
-    'roslyn',
+    -- Note: roslyn is configured below but not auto-installed via Mason
   },
   automatic_enable = false,
 })
@@ -421,12 +421,15 @@ local lsp_server_configs = {
 
   omnisharp = {
     root_dir = lspconfig_util.root_pattern('*.sln', '*.csproj', 'omnisharp.json', 'function.json'),
-    handlers = {
-      ['textDocument/definition'] = require('omnisharp_extended').definition_handler,
-      ['textDocument/typeDefinition'] = require('omnisharp_extended').type_definition_handler,
-      ['textDocument/references'] = require('omnisharp_extended').references_handler,
-      ['textDocument/implementation'] = require('omnisharp_extended').implementation_handler,
-    },
+    handlers = (function()
+      local omnisharp_extended = require('omnisharp-extended-lsp')
+      return {
+        ['textDocument/definition'] = omnisharp_extended.definition_handler,
+        ['textDocument/typeDefinition'] = omnisharp_extended.type_definition_handler,
+        ['textDocument/references'] = omnisharp_extended.references_handler,
+        ['textDocument/implementation'] = omnisharp_extended.implementation_handler,
+      }
+    end)(),
     enable_roslyn_analyzers = true,
     organize_imports_on_format = true,
     enable_import_completion = true,
@@ -452,35 +455,35 @@ local lsp_server_configs = {
     end,
   },
 
-  roslyn = {
-    root_dir = lspconfig_util.root_pattern('*.sln', '*.csproj'),
-    filetypes = { 'cs', 'vb' },
-    settings = {
-      ['csharp|inlay_hints'] = {
-        csharp_enable_inlay_hints_for_implicit_object_creation = true,
-        csharp_enable_inlay_hints_for_implicit_variable_types = true,
-        csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-        csharp_enable_inlay_hints_for_types = true,
-        dotnet_enable_inlay_hints_for_indexer_parameters = true,
-        dotnet_enable_inlay_hints_for_literal_parameters = true,
-        dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-        dotnet_enable_inlay_hints_for_other_parameters = true,
-        dotnet_enable_inlay_hints_for_parameters = true,
-        dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-        dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-        dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-      },
-      ['csharp|code_lens'] = {
-        dotnet_enable_references_code_lens = true,
-        dotnet_enable_tests_code_lens = true,
-      },
-      ['csharp|completion'] = {
-        dotnet_provide_regex_completions = true,
-        dotnet_show_completion_items_from_unimported_namespaces = true,
-        dotnet_show_name_completion_suggestions = true,
-      },
-    },
-  },
+  -- roslyn = {
+  --   root_dir = lspconfig_util.root_pattern('*.sln', '*.csproj'),
+  --   filetypes = { 'cs', 'vb' },
+  --   settings = {
+  --     ['csharp|inlay_hints'] = {
+  --       csharp_enable_inlay_hints_for_implicit_object_creation = true,
+  --       csharp_enable_inlay_hints_for_implicit_variable_types = true,
+  --       csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+  --       csharp_enable_inlay_hints_for_types = true,
+  --       dotnet_enable_inlay_hints_for_indexer_parameters = true,
+  --       dotnet_enable_inlay_hints_for_literal_parameters = true,
+  --       dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+  --       dotnet_enable_inlay_hints_for_other_parameters = true,
+  --       dotnet_enable_inlay_hints_for_parameters = true,
+  --       dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+  --       dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+  --       dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+  --     },
+  --     ['csharp|code_lens'] = {
+  --       dotnet_enable_references_code_lens = true,
+  --       dotnet_enable_tests_code_lens = true,
+  --     },
+  --     ['csharp|completion'] = {
+  --       dotnet_provide_regex_completions = true,
+  --       dotnet_show_completion_items_from_unimported_namespaces = true,
+  --       dotnet_show_name_completion_suggestions = true,
+  --     },
+  --   },
+  -- },
 
   bashls = {},
   cssls = {},
